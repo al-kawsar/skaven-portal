9a7273bdcf95e79d5c628fa68ad5045f47dbf9b3
Raihan Alkawsar <tlod9906@gmail.com>
2024-08-10 12:15:00 +0800

    chore(app): update middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\checkRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,20 +12,27 @@ use Inertia\Inertia;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
+
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
-        $middleware->redirectGuestsTo(fn(Request $request) => route('auth.login.index'));
+        $middleware->alias([
+            'role' => checkRole::class,
+        ]);
+        $middleware->redirectGuestsTo(function (Request $request) {
+            return route('auth.login.index');
+        });
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
             if (!app()->environment(['local', 'testing']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
-                return Inertia::render('Error', ['status' => $response->getStatusCode()])
+                return Inertia::render('Errors/Index', ['status' => $response->getStatusCode()])
                     ->toResponse($request)
                     ->setStatusCode($response->getStatusCode());
             } elseif ($response->getStatusCode() === 419) {
