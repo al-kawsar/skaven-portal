17c46518f8c3d0a1a04798bbe7ad6b4b5abb05af
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-10 10:40:00 +0800

    fix: update app.blade.php layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="icon"
+        href="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png"
+        type="image/x-icon" />
     @inertiaHead
     @routes
     @vite(['resources/js/app.js', 'resources/css/app.css'])
