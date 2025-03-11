ffd902f3177590588cc6c6f4f3e86482cfd37040
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-11 10:40:00 +0800

    feat: add Teacher model

---
+++ app/Models/Teacher.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
+
+class Teacher extends Model
+{
+    use HasFactory, Searchable;
+
+    protected $guarded = ['id'];
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+}
