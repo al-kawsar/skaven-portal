606aa881eaf7a10398faae1f9fb82d1fa21b3d1a
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-12 10:00:00 +0800

    feat: add Classes model

---
+++ app/Models/Classes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
+
+class Classes extends Model
+{
+    use HasFactory, Searchable;
+
+    protected $guarded = ['id'];
+
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class, 'wali_kelas_id');
+    }
+}
