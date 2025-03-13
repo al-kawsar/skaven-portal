94a0539e1717b182f58d521601ae7682b86188bf
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-13 10:30:00 +0800

    refactor: update schedules migration

---
+++ database/migrations/2024_07_28_131100_create_schedules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->string('day', 10);
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->unsignedBigInteger('class_id');
+            $table->unsignedBigInteger('subject_id');
+            $table->unsignedBigInteger('teacher_id');
+            $table->timestamps();
+
+            $table->foreign('class_id')->references('id')->on('classes');
+            $table->foreign('subject_id')->references('id')->on('subjects');
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
