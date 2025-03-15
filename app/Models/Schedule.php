96398f580a9986dc87eee2c8058502dffa8271c7
Raihan Alkawsar <tlod9906@gmail.com>
2025-03-15 10:50:00 +0800

    feat: add Schedule model

---
+++ app/Models/Schedule.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'day',
+        'start_time',
+        'end_time',
+        'class_id',
+        'subject_id',
+        'teacher_id',
+    ];
+
+    public function class()
+    {
+        return $this->belongsTo(Classes::class);
+    }
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+}
